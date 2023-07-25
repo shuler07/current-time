8106de53fdab6618dfa5c445a9e5d20e1a36ea49
shuler07 <140427227+shuler07@users.noreply.github.com>
2023-07-25 11:11:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-setInterval(() => {
-  const time = document.querySelector("#time");
-  let date = new Date();
-  let years = date.GetFullYear();
-  let months = date.GetMonth();
-  let days = date.GetDate();
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
-  if (months < 10) {
-    months = "0" + months;
-  }
-  if (days < 10) {
-    days = "0" + days;
-  }
-  if (hours < 10) {
-    hours = "0" + hours;
-  }
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
-  time.textContent = years + "-" + months + "-" + days + "-" + hours + "-" + minutes + "-" + seconds;
-}, 1000)
+let time = document.querySelector("#time");
+function clock() { time.textContent = new Date() }
+setInterval(() => { clock() }, 1000);
