148fc0e51d6a6568c02a11baef7a0adcaa7cf303
shuler07 <140427227+shuler07@users.noreply.github.com>
2023-07-27 20:29:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
 let year = document.querySelector("#years");
+let year = document.querySelector("#years");
 let month = document.querySelector("#monthes");
 let day = document.querySelector("#days");
 let hour = document.querySelector("#hours");
 let minute = document.querySelector("#minutes");
 let second = document.querySelector("#seconds");
 
-function clock() {
-    let date = new Date();
-
-    year.textContent = date.getFullYear();
-    month.textContent = date.getMonth();
-    day.textContent = date.getDay();
-    hour.textContent = date.getHours();
-    minute.textContent = date.getMinutes();
-    second.textContent = date.getSeconds();
-    if (month.textContent < 10) {
-        month.textContent = "0" + date.getMonth();
-    }
-    if (day.textContent < 10) {
-        day.textContent = "0" + date.getDay();
-    }
-    if (hour.textContent < 10) {
-        hour.textContent = "0" + date.getHours();
-    }
-    if (minute.textContent < 10) {
-        minute.textContent = "0" + date.getMinutes();
-    }
-    if (second.textContent < 10) {
-        second.textContent = "0" + date.getSeconds();
-    }
+year.textContent = date.getFullYear();
+month.textContent = date.getMonth();
+day.textContent = date.getDate();
+hour.textContent = date.getHours();
+minute.textContent = date.getMinutes();
+second.textContent = date.getSeconds();
+if (month.textContent < 10) {
+    month.textContent = "0" + date.getMonth();
 }
-
-setTimeout(clock(), 1000);
+if (day.textContent < 10) {
+    day.textContent = "0" + date.getDay();
+}
+if (hour.textContent < 10) {
+    hour.textContent = "0" + date.getHours();
+}
+if (minute.textContent < 10) {
+    minute.textContent = "0" + date.getMinutes();
+}
+if (second.textContent < 10) {
+    second.textContent = "0" + date.getSeconds();
+}
